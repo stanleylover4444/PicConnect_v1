699f7d73055fdd5158dafb1824e09f3aef9759cc
stanleylover4444 <StanleyInTech@gmail.com>
2024-07-25 15:18:07 +0700

    UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,8 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.viewpager2:viewpager2:1.1.0")
     implementation("com.github.bumptech.glide:glide:4.15.0")
-  
+
+    implementation("com.google.android.material:material:1.4.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
